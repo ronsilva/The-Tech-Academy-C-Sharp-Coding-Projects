cc3d62743145eb2b29863e9af2847bd647104a5d
Ron Silva <ron_silva@outlook.com>
2018-12-21 16:36:44 -0800

    Added Newsletter Project

---
+++ NewsletterAppMVC/NewsletterAppMVC/Controllers/AdminController.cs
@@ -0,0 +1,49 @@
+﻿using NewsletterAppMVC.Models;
+using NewsletterAppMVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewsletterAppMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (var db = new NewsletterEntities())
+            {
+                //var signups = db.SignUps.Where(x => x.Removed == null).ToList();
+                var signups = (from c in db.SignUps
+                               where c.Removed == null
+                               select c).ToList();
+                var signupVms = new List<SignupVm>();
+                foreach (var signup in signups)
+                {
+                    var signupVm = new SignupVm()
+                    {
+                        Id = signup.Id,
+                        FirstName = signup.FirstName,
+                        LastName = signup.LastName,
+                        EmailAddress = signup.EmailAddress
+                    };
+                    signupVms.Add(signupVm);
+                }
+                return View(signupVms);
+            }
+        }
+
+        public ActionResult Unsubscribe(int Id)
+        {
+            using(var db = new NewsletterEntities())
+            {
+                var signup = db.SignUps.Find(Id);
+                signup.Removed = DateTime.Now;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ NewsletterAppMVC/NewsletterAppMVC/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using NewsletterAppMVC.Models;
+using System.Web.Mvc;
+
+namespace NewsletterAppMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signup(string firstName, string lastName, string emailAddress)
+        {
+            if(string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress))
+            {
+               return View("~/Views/Shared/Error.cshtml");
+            }
+            else
+            {
+                using (NewsletterEntities db = new NewsletterEntities())
+                {
+                    var signup = new SignUp()
+                    {
+                        FirstName = firstName,
+                        LastName = lastName,
+                        EmailAddress = emailAddress
+                    };
+                    db.SignUps.Add(signup);
+                    db.SaveChanges();
+                }
+                return View("Success");
+            }
+        }
+    }
+}
